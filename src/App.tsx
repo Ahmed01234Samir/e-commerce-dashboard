733d740911d0b4710cb078079dc4b6d4a238765f
Ahmed01234Samir <zombieahmed2002@gmail.com>
2024-08-01 21:29:18 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,76 @@
-function App() {
+import Home from "./Pages/Home/Home";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Users from "./Pages/Users/Users";
+import Products from "./Pages/Products/Products";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
+import Menu from "./Components/Menu/Menu";
+import Login from "./Pages/Login/Login";
+import "./styles/global.scss";
+import User from "./Pages/user/User";
+import Product from "./Pages/product/Product";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
-  return (
-    <div>Hello World</div>
-  )
+
+const queryClient = new QueryClient();
+
+function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <QueryClientProvider client={queryClient}>
+              <Outlet />
+            </QueryClientProvider>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+        {
+          path: "/users/:id",
+          element: <User />,
+        },
+        {
+          path: "/products/:id",
+          element: <Product />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./Footer.scss"
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>lamadmin</span>
+      <span>© Lama Dev Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
